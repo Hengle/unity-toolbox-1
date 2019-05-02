40c35b14673a91ec372921bd63d5f1a730b1ce87
Anton Pantev <antonpantev123@gmail.com>
2019-05-01 20:34:59 -0400

    Adding MoveGraph.

--- Assets/Scripts/Toolbox/Pathfinding/Graph.cs
+++ Assets/Scripts/Toolbox/Pathfinding/Graph.cs
@@ -19,25 +19,16 @@ namespace Toolbox
     public class FourDirectionGraph : IGraph
     {
         Tilemap map;
-        BoundsInt bounds;
 
         /// <summary>
         /// Creates a new FourDirectionalGraph using the tilemap's cell bounds
         /// as the graph bounds. If the tilemap does not have tiles on its
         /// intended boundary then the graph bounds could be smaller than it
-        /// should be and you should use the other constructor to specify the
-        /// graph bounds.
+        /// should be.
         /// </summary>
         public FourDirectionGraph(Tilemap map)
         {
             this.map = map;
-            bounds = map.cellBounds;
-        }
-
-        public FourDirectionGraph(Tilemap map, BoundsInt bounds)
-        {
-            this.map = map;
-            this.bounds = bounds;
         }
 
         public IEnumerable<Vector3Int> Neighbors(Vector3Int v)
@@ -46,9 +37,57 @@ namespace Toolbox
             {
                 Vector3Int next = v + dir;
 
-                if (bounds.Contains(next) && map.GetTile(next) == null)
+                if (map.IsCellEmpty(next))
                 {
                     yield return next;
+                }
+            }
+        }
+
+        public float Cost(Vector3Int a, Vector3Int b)
+        {
+            return Vector3Int.Distance(a, b);
+        }
+    }
+
+    /// <summary>
+    /// Creates a Graph where characters can move in all 8 directions but cannot
+    /// go diagonal if it is across a corner.
+    /// </summary>
+    public class MoveGraph : IGraph
+    {
+        Tilemap map;
+
+        public MoveGraph(Tilemap map)
+        {
+            this.map = map;
+        }
+
+        public IEnumerable<Vector3Int> Neighbors(Vector3Int v)
+        {
+            foreach (Vector3Int dir in Utils.FourDirections)
+            {
+                Vector3Int next = v + dir;
+
+                if (map.IsCellEmpty(next))
+                {
+                    yield return next;
+                }
+            }
+
+            foreach (Vector3Int dir in Utils.DiagonalDirections)
+            {
+                Vector3Int next = v + dir;
+
+                if (map.IsCellEmpty(next))
+                {
+                    Vector3Int adjacent1 = v + new Vector3Int(dir.x, 0, 0);
+                    Vector3Int adjacent2 = v + new Vector3Int(0, dir.y, 0);
+
+                    if (map.IsCellEmpty(adjacent1) && map.IsCellEmpty(adjacent2))
+                    {
+                        yield return next;
+                    }
                 }
             }
         }
